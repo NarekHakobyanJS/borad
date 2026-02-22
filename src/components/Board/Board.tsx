727104c880567df4bd3160d21c5b3ff29388fe31
NarekHakobyanJS <haobyan95narek@gmail.com>
2026-02-22 12:15:15 +0400

    activeCell used

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,14 +1,28 @@
+import { useState } from 'react'
 import { cells } from '../../constants/cells'
 import { Cell } from '../Cell/Cell'
 
 import './Board.css'
 
 export const Board = () => {
+
+  const [activeCell, setActiveCell] = useState<null | number>(null)
+
+  const changeActiveCell = (newActiveElement : number) => {
+    setActiveCell(newActiveElement)
+  }
+  
   return (
     <div className='Board'>
       {
-        cells.map((cell) => {
-          return <Cell />
+        cells.map((cell, index) => {
+          return <Cell 
+          key={cell} 
+          cell={cell}
+          index={index + 1}
+          activeCell={activeCell}
+          changeActiveCell={changeActiveCell}
+          />
         })
       }
     </div>
--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -1,9 +1,20 @@
 import './Cell.css'
 
-export const Cell = () => {
+type CellPropsType = {
+  cell : number
+  activeCell : null | number
+  index : number
+  changeActiveCell : (newActiveElement : number) => void
+}
+export const Cell = ({cell, activeCell, changeActiveCell, index} : CellPropsType) => {
+    
   return (
-    <div className='Cell'>
-      
+    <div 
+    onClick={() => changeActiveCell(cell)}
+    className={`Cell ${activeCell === index ? "ActiveCell" : ""}`}
+    
+    >
+      {cell}
     </div>
   )
 }
